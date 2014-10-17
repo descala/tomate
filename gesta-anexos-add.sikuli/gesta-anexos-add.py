0e1a87169070905a4ef24600fecb0fdbdf032927
David Escala <descala@ingent.net>
2014-10-17 11:21:23 +0200

    Adds missing png
    BTW see Sikuli bug https://bugs.launchpad.net/sikuli/+bug/1331924

--- gesta-anexos-add.sikuli/gesta-anexos-add.py
+++ gesta-anexos-add.sikuli/gesta-anexos-add.py
@@ -3,6 +3,6 @@ n=int(ns)
 i=0;
 while i < n:
    click("1412366416956.png")
-   sleep(1)
+   sleep(0.5)
    i=i+1
    
--- gesta-anexos-upload.sikuli/gesta-anexos-upload.py
+++ gesta-anexos-upload.sikuli/gesta-anexos-upload.py
@@ -1,51 +1,37 @@
-#story = inputText("Fitxers a pujar. Llista de noms. La última línia de la pantalla serà la primera.")
-#story="06_registral-20.jpg\n06_registral-21.jpg\n06_registral-22.jpg"
+story=r"""file1.jpg
+file2.jpg
+file3.jpg"""
 
-story=r"""99_aux-0.jpg
-99_aux-1.jpg
-99_aux-2.jpg
-99_aux-3.jpg
-99_aux-4.jpg
-99_aux-5.jpg
-99_aux-6.jpg
-99_aux-7.jpg
-99_aux-8.jpg
-99_aux-9.jpg
-99_aux-10.jpg
-99_aux-11.jpg
-99_aux-12.jpg
-99_aux-13.jpg
-99_aux-14.jpg
-99_aux-15.jpg
-99_aux-16.jpg
-99_aux-17.jpg"""
 lines = story.split("\n")
 
-setAutoWaitTimeout(10)
+setAutoWaitTimeout(30)
 
 for line in lines:
-    wait("1412359466523.png")
-    sleep(0.5)    
-    click("1412359466523.png")
-    sleep(0.5)
-    wait("1412359517722.png")
-    click("1412359517722.png")
-    wait("1412359617626.png")
-    click("1412359617626.png")
-    wait("1412359724889.png")
-    click("1412359724889.png")
+    wait("1413531652443.png")
+    sleep(0.1)    
+    click("1413531652443.png")
     sleep(0.1)
+    wait("1413531762414.png")
+    click("1413531762414.png")
+    wait("1413531816489.png")
+    click("1413531816489.png")
+    wait("1413531916212.png")
+    click("1413531916212.png")
+    sleep(0.2)
     type(line)
-    click("1412360881105.png")
-    wait("1412360931689.png")
-    click("1412360931689.png")
-    wait("1412361051064.png")
-    click("1412361063928.png")
-    wait("1412359466523.png")    
-    click("1412359466523.png")
-    sleep(0.5)
-    wait("1412361276270.png")
-    click("1412359466523.png")
-    sleep(0.5)
+    click("1413531952037.png")
+    wait("1413531981678.png")
+    click("1413531981678.png")
+    sleep(0.2)
+    wait("1413535294437.png")
+    wait("1413534910431.png")
+    click("1413532071505.png")
+    wait("1413531652443.png")
+    sleep(0.1)
+    click("1413531652443.png")
+    sleep(0.2)
+    wait("1413532140004.png")
+    click("1413531652443.png")
+    sleep(0.2)
     type(Key.DOWN)
-    sleep(2)
+    sleep(0.6)
